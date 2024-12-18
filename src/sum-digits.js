ab5b0ce137f11650354e714544a38badf4ccae87
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-12-18 21:52:34 +0700

    feat: add part of solution

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n >= 0 && n <= 9) {
+    return n;
+  }
+  const digits = [];
+  while (n) {
+    digits.push(n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  return getSumOfDigits(digits.reduce((acc, digit) => acc + digit))
 }
+
+console.log(getSumOfDigits(91))
 
 module.exports = {
   getSumOfDigits
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,78 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
+
+function deleteControl (index, array) {
+  array.splice(index, 1);
+  deletedIndex = null;
+}
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
+ * --discard-next excludes the next element of the array from the transformed array.
+ *
+ * --discard-prev excludes the previous element of the array from the transformed array.
+ *
+ * --double-next duplicates the next element of the array in the transformed array.
+ *
+ * --double-prev duplicates the previous element of the array in the transformed array.
+ *
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  const transformedArray = [...arr];
+  let deletedIndex = null;
+  for (let index = 0; index < transformedArray.length; index++) {
+    const array = transformedArray;
+    const item = transformedArray[index]
+    if (item === '--double-next') {
+      if (index + 1 > array.length - 1 || index + 1 === deletedIndex) {
+        deleteControl(index, array)
+      } else {
+        array.splice(index, 1);
+        array.splice(index, 0, array[index]);
+      }
+    }
+    if (item === '--double-prev') {
+      if (index - 1 < 0 || index - 1 === deletedIndex) {
+        deleteControl(index, array)
+      } else {
+        array.splice(index, 1);
+        array.splice(index, 0, array[index - 1]);
+      }
+    }
+    if (item === '--discard-prev') {
+      if (index - 1 < 0 || index - 1 === deletedIndex) {
+        deleteControl(index, array)
+      } else {
+        array.splice(index - 1, 2);
+        deletedIndex = index - 1;
+      }
+      index --
+    }
+    if (item === '--discard-next') {
+      if (index + 1 > array.length - 1 || index + 1 === deletedIndex) {
+        deleteControl(index, array)
+      } else {
+        array.splice(index, 2);
+        deletedIndex = index - 1;
+      }
+      index --
+
+    }
+  }
+  return transformedArray;
 }
 
 module.exports = {
